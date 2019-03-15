250629c401c51a9c5b0fa36a4625d99868e72c7c
apidcloud <luisapidcloud@hotmail.com>
2019-03-14 19:39:36 -0600

    chore: upgrade deps

--- packages/core/src/coreExample.ts
+++ packages/core/src/coreExample.ts
@@ -1,30 +1,9 @@
-import { Simple, Person } from "@scarlett-game-studio/scarlett-commons";
-import { TypedJSON } from "typedjson";
+import { Simple } from "@scarlett-game-studio/scarlett-commons";
 
 let x: Simple = new Simple();
 let y: string = x.methodB();
 
 console.log(y);
-
-//const p = new Person("Luis");
-//const data = JSON.parse(JSON.stringify(p));
-
-//console.log(data);
-
-let serializer = new TypedJSON(Person);
-let object = new Person("Luís");
-
-console.log("original", object);
-
-let json = serializer.stringify(object);
-
-console.log(json);
-
-let object2 = serializer.parse(json);
-
-console.log("restored", object2);
-
-console.log("instance of", object2 instanceof Person); // true
 
 export function fn() {
   return "Hello world";
