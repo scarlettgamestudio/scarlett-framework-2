d6c004b8895756c2c4e42fe0929481ebe0d60182
apidcloud <luisapidcloud@hotmail.com>
2019-03-15 19:10:23 -0600

    chore: cleanup

--- packages/common/src/person.ts
+++ packages/common/src/person.ts
@@ -1,63 +1,6 @@
-//import { serializable } from "./serializers";
 import Simple from "./simple";
-//import { logClass } from "./logClass";
 import { jsonObject, jsonMember, TypedJSON, toJson } from "typedjson";
 
-/** 
-// eslint-disable-next-line
-interface ParameterlessConstructor<T> {
-  //eslint-disable-next-line
-  new (): T;
-}
-
-// @ts-ignore
-// eslint-disable-next-line
-function toJson<T>(target: ParameterlessConstructor<T>) {
-  target.prototype.toJSON = function() {
-    return TypedJSON.toPlainJson(this, target);
-  };
-}
-**/
-
-/** 
- * Or through the following decorator.
- * But this one might yield some issues when minifying or uglifying
- * because it relies on prototype.constructor.
- function toJson(target: Function) {
-  target.prototype.toJSON = function() {
-    return TypedJSON.toPlainJson(this, target.prototype.constructor);
-  };
-}
-*/
-
-/**
- * Can also be accomplished through an abstract class 
- * that must be extended as follows:
- * Class Person extends Serializable<Person> {
- *    constructor(name?: string) {
-        super(Person);
-        this._name = name || "some default name 2";
-      }
- * }
- */
-
-/** 
- * The actual abstract class, but it's not too elegant
- * and requires extending a class.
-  abstract class Serializable<T> {
-    // eslint-disable-next-line
-    protected constructor(protected ctor: ParameterlessConstructor<T>) {}
-
-    toJson(instance: T): string {
-      const serializer = new TypedJSON(this.ctor);
-
-      const result = serializer.stringify(instance);
-      return result;
-    }
-  }
-**/
-
-//@serializable()
 @toJson
 @jsonObject
 class Person {
@@ -93,10 +36,9 @@ class Person {
 
     console.log("restored", object2);
 
-    console.log("instance of", object2 instanceof Person); // true
+    console.log("instance of", object2 instanceof Person);
   }
 
-  //@serialize("Language")
   get lang() {
     return "JavaScript";
   }
